eebb71677f727a878781cba4e6b7b2e2fb0aa457
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-04-05 09:01:37 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+import os
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "welcome to the home page."
+
+@app.route("/h1/")
+def who():
+    return "Who are toy?"
+
+
+@app.route("/h1/<username>")
+def greet(username):
+    return f"hi there, {username}!"
+
+@app.route("/index")
+def index():
+    return render_template("public/index.xhtml")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
