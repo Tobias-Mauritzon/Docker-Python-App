12ad4f8fb2c789db2044c9a4c944c52cb4276bc6
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-04-18 22:13:14 +0200

    testing how youtubes api work

--- api_test.py
+++ api_test.py
@@ -6,12 +6,26 @@ steamAPIResponse = requests.get("https://api.steampowered.com/")
 
 print(steamAPIResponse.status_code)
 
-key = os.environ.get("Youtube_API_KEY")
-print(key)
-youtubeRespone = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key="+key)
 
-print(youtubeRespone.json())
 
+
+
+
+
+def search_test():
+  key = os.environ.get("Youtube_API_KEY")
+  pyoutubeRespone = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key="+key)
+  print(youtubeRespone.json())
+  print("Hej")
+  print(youtubeRespone)
+
+def search_top_video(channel):
+  key = os.environ.get("Youtube_API_KEY")
+  youtubeRespone = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId="+channel+"&maxResults=10&order=viewCount&key="+key)
+  print(youtubeRespone.json())
+
+
+search_top_video('Linus Tech Tips')
 
 '''
   requests.get('https://youtube.googleapis.com/youtube/v3/search?channelId=Linus%20Tech%20Tips%20&key=[YOUR_API_KEY]' \
